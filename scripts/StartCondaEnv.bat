ec91615e3e4381ae22d768f099280d99532204e6
Marius-Vlad Dumitru <seyino.engineer@gmail.com>
2025-03-12 00:16:12 +0200

    Update.

--- scripts/StartCondaEnv.bat
+++ scripts/StartCondaEnv.bat
@@ -1,5 +1,7 @@
 :: Start the specific conda environment for this project. Makes it a little faster to start working.
 
+@echo off
+
 :: Call my own script that activates miniconda. MinicondaSetup must already be in PATH.
 call MinicondaSetup.bat
 

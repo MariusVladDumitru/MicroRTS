8cf429b33084c9350ca60f0c9faeee9478483c00
Marius-Vlad Dumitru <seyino.engineer@gmail.com>
2025-03-23 00:05:40 +0200

    Added MicroRTS-clean.sh

---
+++ scripts/MicroRTS-clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Run MicroRTS game
+
+# The root folder of MicroRTS project
+export ROOT=$PROJECTS/Active/Dissertation_Thesis/Code/MicroRTS
+cd $ROOT/build
+rm -r ./* # delete all contents of the build foder

e4f24bc2470aa4861e6cba55960b9e2ce2285234
Marius-Vlad Dumitru <seyino.engineer@gmail.com>
2025-04-01 15:48:59 +0300

    Added scripts/MicroRTS-run.ps1, scripts/MicroRTS-run-gui.ps1.

---
+++ scripts/MicroRTS-run.ps1
@@ -0,0 +1,13 @@
+# Run MicroRTS game in Windows powershell
+
+# The root folder of MicroRTS project
+# The W: drive must be substed
+$env:ROOT= 'W:\Active\Dissertation_Thesis\Code\MicroRTS
+
+# Go to build folder
+Set-Location $ROOT/build
+
+# JAVA JDK must be in $env:PATH
+# MicroRTS.jar must be in build
+# @args - pass all arguments the script gets to MicroRTS.jar
+& java -cp ./MicroRTS.jar rts.MicroRTS @args
